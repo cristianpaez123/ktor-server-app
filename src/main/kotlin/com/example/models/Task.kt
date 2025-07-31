616b153b1524c9919e17d68bfd9e955d40a540a1
Cristian Paez <cristianpaezguerrero@gmail.com>
2025-07-30 21:42:19 -0500

    se actualiza

--- src/main/kotlin/com/example/models/Task.kt
+++ src/main/kotlin/com/example/models/Task.kt
@@ -5,7 +5,7 @@ import java.util.*
 
 @Serializable
 data class Task(
-    val id: String = UUID.randomUUID().toString(),
+    val id: String ,
     val title: String,
     val description: String,
     val isCompleted: Boolean = false
